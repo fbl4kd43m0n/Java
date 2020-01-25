6775d8077d9de2aa2baf4a1cd49409502ffe5182
Franciny Salles <35750347+fbl4kd43m0n@users.noreply.github.com>
2020-01-25 15:38:49 -0300

    Add files via upload

---
+++ controle/DesafioIf.java
@@ -0,0 +1,21 @@
+package controle;
+
+public class DesafioIf {
+
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		
+		if (nota >= 9.0)
+			
+			//;
+			//não usar ; em estruturas de controle (tem uma exceção)
+			
+		{
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera!!!");
+		}
+		
+	}
+	
+}
